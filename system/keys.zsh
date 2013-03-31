a23cd42885f2009c9e501aece1e9e11624d3f047
Justas Palumickas <justas@elish.lt>
2013-03-31 22:21:00 +0300

    Adding authme to keys and changing to rsa from dsa.

--- system/keys.zsh
+++ system/keys.zsh
@@ -1,2 +1,11 @@
 # Pipe my public key to my clipboard. Fuck you, pay me.
-alias pubkey="more ~/.ssh/id_dsa.public | pbcopy | echo '=> Public key copied to pasteboard.'"
+alias pubkey="more ~/.ssh/id_rsa.public | pbcopy | echo '=> Public key copied to pasteboard.'"
+
+# From https://github.com/jnunemaker/dotfiles/blob/master/bash/functions
+# Appends your key to a server's authorized keys file
+function authme
+{
+  if [ "$1" != "" ]; then
+    ssh $1 'cat >>.ssh/authorized_keys' <~/.ssh/id_rsa.pub
+  fi
+}
