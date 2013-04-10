8959717ae28cf85aa1b83ca583e2c182cab938d3
Justas Palumickas <justas@elish.lt>
2013-04-10 16:53:04 +0300

    Update keys file.

--- system/keys.zsh
+++ system/keys.zsh
@@ -1,5 +1,5 @@
 # Pipe my public key to my clipboard. Fuck you, pay me.
-alias pubkey="more ~/.ssh/id_rsa.public | pbcopy | echo '=> Public key copied to pasteboard.'"
+alias pubkey="more ~/.ssh/id_rsa.pub | pbcopy | echo '=> Public key copied to pasteboard.'"
 
 # From https://github.com/jnunemaker/dotfiles/blob/master/bash/functions
 # Appends your key to a server's authorized keys file
