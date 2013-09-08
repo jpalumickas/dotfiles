2d6310bdc8e4a14c24d6df1225fd38014c695543
Justas Palumickas <justas@elish.lt>
2013-09-08 03:06:07 +0300

    Add new aliases to system

--- system/aliases.zsh
+++ system/aliases.zsh
@@ -8,3 +8,16 @@ then
   alias ll="gls -l --color"
   alias la='gls -A --color'
 fi
+
+# Get week number
+alias week='date +%V'
+
+# IP addresses
+alias ip="dig +short myip.opendns.com @resolver1.opendns.com"
+alias localip="ipconfig getifaddr en1"
+
+# Clean up LaunchServices to remove duplicates in the “Open With” menu
+alias lscleanup="/System/Library/Frameworks/CoreServices.framework/Frameworks/LaunchServices.framework/Support/lsregister -kill -r -domain local -domain system -domain user && killall Finder"
+
+# Recursively delete `.DS_Store` files
+alias cleanup="find . -type f -name '*.DS_Store' -ls -delete"
