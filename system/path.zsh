d645fafaf25f1d49a557de4de140f8f282ebc27e
Justas Palumickas <jpalumickas@gmail.com>
2014-02-16 23:31:56 +0200

    Add X11 to path

--- system/path.zsh
+++ system/path.zsh
@@ -1,3 +1,6 @@
+# X11 Path
+export PATH="/usr/X11/bin:$PATH"
+
 export PATH="./bin:$HOME/.rbenv/shims:/usr/local/bin:/usr/local/sbin:$HOME/.sfs:$ZSH/bin:$PATH"
 
 export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
