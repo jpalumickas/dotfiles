760d41a82af58ddff15ed1b5b91254ed8c472e43
Justas Palumickas <jpalumickas@gmail.com>
2014-09-17 09:42:31 +0300

    Add some node installs

--- node/install.sh
+++ node/install.sh
@@ -1,5 +1,15 @@
+# Developer tools
 npm install -g grunt-cli
 npm install -g bower
-npm install -g coffeelint
-npm install -g jshint
-npm install -g csslint
+
+# Code linters
+npm install -g coffeelint # CoffeeScript linter.
+npm install -g jshint # JavaScript linter.
+npm install -g csslint # CSS linter.
+
+# Utilities
+npm install -g public-ip # Show public ip.
+npm install -g internal-ip # Show iternal ip.
+npm install -g peerflix # Show torrent movies without downloading.
+npm install -g pageres # Capture screenshots of websites in various resolutions.
+npm install -g is-up # Check whether a website is up or down.
