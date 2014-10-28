69e0b8336638d6442bd668fd8c7a4925093c3dd1
Justas Palumickas <jpalumickas@gmail.com>
2014-10-28 14:04:16 +0200

    Update node installs and Atom settings

--- node/install.sh
+++ node/install.sh
@@ -6,6 +6,7 @@ npm install -g bower
 npm install -g coffeelint # CoffeeScript linter.
 npm install -g jshint # JavaScript linter.
 npm install -g csslint # CSS linter.
+npm install -g bootlint # Bootstrap linter.
 
 # Utilities
 npm install -g public-ip # Show public ip.
@@ -13,4 +14,4 @@ npm install -g internal-ip # Show iternal ip.
 npm install -g peerflix # Show torrent movies without downloading.
 npm install -g pageres # Capture screenshots of websites in various resolutions.
 npm install -g is-up # Check whether a website is up or down.
-npm install -g trash # Remove files safe
+npm install -g trash # Remove files safe.
