c8f052c6d1d91eabc0a2a2ec7a555a76a76bb847
Justas Palumickas <jpalumickas@gmail.com>
2015-07-07 18:12:05 +0300

    Install base16-shell with dotfiles

--- ruby/install.sh
+++ ruby/install.sh
@@ -23,6 +23,7 @@ then
   brew install heroku-toolbelt
 fi
 
+echo "  Installing heroku-config plugin for you."
 heroku plugins:install git://github.com/ddollar/heroku-config.git
 
 
@@ -32,4 +33,7 @@ rbenv_default_gems_dir=~/.rbenv/plugins/rbenv-default-gems
 if [ ! -d "$rbenv_default_gems_dir" ]; then
   echo "  Installing rbenv-default-gems for you."
   git clone https://github.com/sstephenson/rbenv-default-gems.git $rbenv_default_gems_dir
+else
+  echo "  Updating rbenv-default-gems for you."
+  cd $rbenv_default_gems_dir && git pull
 fi
