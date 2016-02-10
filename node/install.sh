bea02ac7c88f5f5e3b71c6096e6549e51d24d0c9
Justas Palumickas <jpalumickas@gmail.com>
2016-02-10 11:19:28 +0200

    Add npm spoof for MAC Spoofing

--- node/install.sh
+++ node/install.sh
@@ -16,3 +16,4 @@ npm install -g peerflix # Show torrent movies without downloading.
 npm install -g pageres # Capture screenshots of websites in various resolutions.
 npm install -g is-up # Check whether a website is up or down.
 npm install -g trash # Remove files safe.
+npm install -g spoof # MAC Spoofing.
