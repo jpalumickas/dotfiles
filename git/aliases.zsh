2bd5efe2b70cdd8a87176dcf912d4501b64488e3
Justas Palumickas <jpalumickas@gmail.com>
2016-02-10 13:03:12 +0200

    Add gac alias

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -18,5 +18,6 @@ alias gco='git checkout'
 alias gcb='git copy-branch-name'
 alias gb='git branch'
 alias gs='git status -sb' # upgrade your git if -sb breaks for you. it's fun.
+alias gac='git add -A && git commit -m'
 alias grm="git status | grep deleted | awk '{\$1=\$2=\"\"; print \$0}' | \
            perl -pe 's/^[ \t]*//' | sed 's/ /\\\\ /g' | xargs git rm"
