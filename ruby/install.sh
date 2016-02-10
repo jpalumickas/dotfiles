2f45723cfaeced0e0a92d669964e0e1721de3cee
Justas Palumickas <jpalumickas@gmail.com>
2016-02-10 12:58:54 +0200

    RBenv is installing through Brewfile

--- ruby/install.sh
+++ ruby/install.sh
@@ -1,17 +1,5 @@
 #!/bin/sh
 
-if test ! $(which rbenv)
-then
-  echo "  Installing rbenv for you."
-  brew install rbenv > /tmp/rbenv-install.log
-fi
-
-if test ! $(which ruby-build)
-then
-  echo "  Installing ruby-build for you."
-  brew install ruby-build > /tmp/ruby-build-install.log
-fi
-
 # Bundler settings
 number_of_cores=$(sysctl -n hw.ncpu)
 bundle config --global jobs $((number_of_cores - 1))
