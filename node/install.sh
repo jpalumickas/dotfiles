4551ed63143e0ff1ef4cf4a44416f3d7bc0166ae
Justas Palumickas <jpalumickas@gmail.com>
2016-04-17 17:42:49 +0300

    Remove unused scripts

--- node/install.sh
+++ node/install.sh
@@ -1,8 +1,9 @@
 # Utilities
-npm install -g public-ip # Show public ip.
 npm install -g internal-ip # Show iternal ip.
-npm install -g peerflix # Show torrent movies without downloading.
-npm install -g pageres # Capture screenshots of websites in various resolutions.
-npm install -g is-up # Check whether a website is up or down.
-npm install -g trash # Remove files safe.
-npm install -g spoof # MAC Spoofing.
+npm install -g is-up       # Check whether a website is up or down.
+npm install -g pageres     # Capture screenshots of websites in various resolutions.
+npm install -g peerflix    # Show torrent movies without downloading.
+npm install -g public-ip   # Show public ip.
+npm install -g spoof       # MAC Spoofing.
+npm install -g trash       # Remove files safe.
+npm install -g urban       # Urban Dictionary
