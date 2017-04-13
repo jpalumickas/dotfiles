5e5b2a23a9230aef55f035d597e1dae353e67cd2
Zach Holman <zach@zachholman.com>
2017-04-13 13:20:49 -0700

    fix the unpushed branch error

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -43,7 +43,7 @@ unpushed () {
 need_push () {
   if [ $($git rev-parse --is-inside-work-tree 2>/dev/null) ]
   then
-    number=$($git cherry -v origin/$(git symbolic-ref --short HEAD) | wc -l | bc)
+    number=$($git cherry -v origin/$(git symbolic-ref --short HEAD) 2>/dev/null | wc -l | bc)
 
     if [[ $number == 0 ]]
     then
