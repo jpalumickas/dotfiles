27f72b6b8f21ea3e0b3087709009596d60be5a8f
Justas Palumickas <jpalumickas@gmail.com>
2019-05-15 17:13:12 +0300

    Small fixes

--- zsh/common.zsh
+++ zsh/common.zsh
@@ -1,12 +1,12 @@
 function colours() {
-for i in {0..255} ; do
-  printf "\x1b[38;5;${i}mcolour${i}\n"
-done
+  for i in {0..255} ; do
+    printf "\x1b[38;5;${i}mcolour${i}\n"
+  done
 }
 
 # find shorthand
 function f() {
-    find . -name "$1"
+  find . -name "$1"
 }
 
 # cd into whatever is the forefront Finder window.
