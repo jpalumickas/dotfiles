ce187942b3a8d6bed0a1cd1bc3491647c6770def
Justas Palumickas <jpalumickas@gmail.com>
2020-05-13 18:20:28 +0300

    Update android path

--- android/path.zsh
+++ android/path.zsh
@@ -1,1 +1,2 @@
-export PATH="/Users/jpalumickas/Library/Android/sdk:$PATH"
+export ANDROID_HOME="/Users/jpalumickas/Library/Android/sdk"
+export PATH="$ANDROID_HOME/platform-tools:$ANDROID_HOME/emulator:$ANDROID_HOME/tools:$ANDROID_HOME/tools/bin:$PATH"
