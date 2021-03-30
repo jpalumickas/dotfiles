4dcb9538a8c4607644a5d897142c698d180089d2
Justas Palumickas <jpalumickas@gmail.com>
2021-03-30 11:05:39 +0300

    Add gcloud autocomplete

--- android/path.zsh
+++ android/path.zsh
@@ -1,2 +1,3 @@
 export ANDROID_HOME="/Users/jpalumickas/Library/Android/sdk"
+export ANDROID_SDK="/Users/jpalumickas/Library/Android/sdk"
 export PATH="$ANDROID_HOME/platform-tools:$ANDROID_HOME/emulator:$ANDROID_HOME/tools:$ANDROID_HOME/tools/bin:$PATH"
