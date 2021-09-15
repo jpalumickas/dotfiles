0200927a04be6822076426b9805457c29fa7a9ca
Justas Palumickas <jpalumickas@gmail.com>
2021-09-15 16:56:48 +0300

    Update ruby version, small other updates

--- node/path.zsh
+++ node/path.zsh
@@ -1,6 +1,6 @@
 # export PATH="/usr/local/share/npm/bin:$PATH"
 
 export NVM_DIR="$HOME/.nvm"
-. "$(brew --prefix nvm)/nvm.sh"
+# . "$(brew --prefix nvm)/nvm.sh"
 
 export PATH="$(yarn global bin):$PATH"
